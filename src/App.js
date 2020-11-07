3b41d81b59ac0a44873f8e251a006c38263e71fb
lukaavbreht <l.avbreht@gmail.com>
2020-11-07 14:29:10 +0100

    added routing

--- src/App.js
+++ src/App.js
@@ -1,22 +1,29 @@
 import logo from './logo.svg';
 import './App.css';
+import 'bootstrap/dist/css/bootstrap.min.css';
+
+import React, { useState } from 'react';
+import { BrowserRouter as Router, Redirect, Route, Switch, useHistory } from 'react-router-dom';
+import NotFound from "./Routes/NotFound";
+import AppContainer from "./Routes/AppContainer";
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <Router>
+          <Switch>
+
+            <Route path="/404" exact={true}>
+              <NotFound />
+            </Route>
+
+            <Route path={'/'}>
+              <AppContainer />
+            </Route>
+
+          </Switch>
+        </Router>
       </header>
     </div>
   );
---
+++ src/Routes/NotFound.js
@@ -0,0 +1,12 @@
+import React from "react";
+
+function NotFound() {
+    return (
+        <div>
+            Not found 404
+        </div>
+
+    );
+}
+
+export default NotFound;
